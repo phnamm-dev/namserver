acc073e7c1d283d6d2de58ebeabc1cd192de0e1c
phnamm-dev <anhtuan18082013@gmail.com>
2026-01-07 23:22:26 +0700

    keyword-map.js

--- chatbot/keyword-map.js
+++ chatbot/keyword-map.js
@@ -1,12 +1,1 @@
-//
-//  _________   ______ _                       _   _                           _   _                 
-// / /  __ \ \  | ___ \ |                     | | | |                         | \ | |                
-//| || /  \/| | | |_/ / |__   __ _ _ __ ___   | |_| | ___   __ _ _ __   __ _  |  \| | __ _ _ __ ___  
-//| || |    | | |  __/| '_ \ / _` | '_ ` _ \  |  _  |/ _ \ / _` | '_ \ / _` | | . ` |/ _` | '_ ` _ \ 
-//| || \__/\| | | |   | | | | (_| | | | | | | | | | | (_) | (_| | | | | (_| | | |\  | (_| | | | | | |
-//| | \____/| | \_|   |_| |_|\__,_|_| |_| |_| \_| |_/\___/ \__,_|_| |_|\__, | \_| \_/\__,_|_| |_| |_|
-// \_\     /_/                                                          __/ |                        
-//                                                                     |___/                         
-//                js file - not obfuscate - - - - - - - > nam dev -__-
-//
 const KEYWORD_MAP={"chào":"chào",hello:"chào",hi:"chào","xin chào":"chào","chào bạn":"chào bạn","bạn là ai":"bạn là ai","tên gì":"bạn là ai","ai tạo":"tác giả","tác giả":"tác giả","phạm hoàng nam":"nam","hoàng nam":"nam","nam 7a2":"nam","nam chatbot offline":"use-offline","toán":"toán","toán học":"toán","học toán":"toán","môn toán":"toán","giải phương trình":"giải phương trình","phương trình":"giải phương trình","hình tròn":"diện tích hình tròn","diện tích":"diện tích hình tròn","chu vi":"diện tích hình tròn","cách học toán":"học toán hiệu quả","học toán sao":"học toán hiệu quả","lập trình":"lập trình",code:"lập trình",coder:"lập trình",programming:"lập trình",html:"html",htm:"html",web:"html",javascript:"javascript",js:"javascript","java script":"javascript",python:"python",py:"python",css:"css",java:"java","học tập":"học tập","học bài":"học tập","phương pháp học":"học tập","thi cử":"thi cử","ôn thi":"thi cử","chuẩn bị thi":"thi cử","mẹo học":"mẹo học bài","học nhanh":"mẹo học bài","học thuộc":"mẹo học bài","cười":"chuyện cười","chuyện cười":"chuyện cười","kể chuyện":"chuyện cười",vui:"chuyện cười","đố":"đố vui","đố vui":"đố vui","đố mẹo":"đố vui","câu đố":"đố vui",chatbot:"chatbot",bot:"chatbot",ai:"ai","trí tuệ nhân tạo":"ai","artificial intelligence":"ai","cảm ơn":"cảm ơn",thanks:"cảm ơn","thank you":"cảm ơn","tạm biệt":"tạm biệt",bye:"tạm biệt",goodbye:"tạm biệt",$demo:"dev",fuck:"cảnh báo",shit:"cảnh báo"};
