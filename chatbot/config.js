e84fe42d37e14c5ff58e8184e49efbe4a655b965
phnamm-dev <anhtuan18082013@gmail.com>
2026-01-07 23:19:20 +0700

    config.js

--- chatbot/config.js
+++ chatbot/config.js
@@ -1,16 +1,1 @@
-// (C) Pham Hoang Nam | config.js : no obfuscate //
-const CONFIG = {
-    APP_NAME: "NamBot Offline AI",
-    VERSION: "v1.054",
-    AUTHOR: "Phạm Hoàng Nam",
-  
-    CLASS: "8A2",
-    MAX_MESSAGES: 100,
-    EMOJI_SUPPORT: true,
-  
-    STORAGE_KEYS: {
-        CHAT_HISTORY: "nam_bot_chat_history_v3",
-        THEME: "nam_bot_theme",
-        SETTINGS: "nam_bot_settings"
-    }
-};
+const CONFIG={APP_NAME:"NamBot Offline AI",VERSION:"v1.054",AUTHOR:"Phạm Hoàng Nam",CLASS:"8A2",MAX_MESSAGES:100,EMOJI_SUPPORT:!0,STORAGE_KEYS:{CHAT_HISTORY:"nam_bot_chat_history_v3",THEME:"nam_bot_theme",SETTINGS:"nam_bot_settings"}};
