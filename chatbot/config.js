fe3c90d3a9dbf5b577f0a6eb059d11416ea96def
phnamm-dev <anhtuan18082013@gmail.com>
2026-01-08 22:08:55 +0700

    config.js

--- chatbot/config.js
+++ chatbot/config.js
@@ -1,1 +1,7 @@
-const CONFIG={APP_NAME:"NamBot Offline AI",VERSION:"v1.054",AUTHOR:"Phạm Hoàng Nam",CLASS:"8A2",MAX_MESSAGES:100,EMOJI_SUPPORT:!0,STORAGE_KEYS:{CHAT_HISTORY:"nam_bot_chat_history_v3",THEME:"nam_bot_theme",SETTINGS:"nam_bot_settings"}};
+/**
+ *    ┏╸┏━╸╺┓   ┏━┓╻ ╻┏━┓┏┳┓   ╻ ╻┏━┓┏━┓┏┓╻┏━╸   ┏┓╻┏━┓┏┳┓
+ *    ┃ ┃   ┃   ┣━┛┣━┫┣━┫┃┃┃   ┣━┫┃ ┃┣━┫┃┗┫┃╺┓   ┃┗┫┣━┫┃┃┃
+ *    ┗╸┗━╸╺┛   ╹  ╹ ╹╹ ╹╹ ╹   ╹ ╹┗━┛╹ ╹╹ ╹┗━┛   ╹ ╹╹ ╹╹ ╹
+ * config config config - - - >  07 thg 01 2025 - - - - > still fake love - - -> upd this after created calculator.js
+ */
+const CONFIG={APP_NAME:"NamBot Offline AI",VERSION:"v1.054",AUTHOR:"Phạm Hoàng Nam",CLASS:"8A2",MAX_MESSAGES:100,EMOJI_SUPPORT:!0,CALCULATOR:!0,THEME_TOGGLE:!0,CHAT_EXPORT:!0,STORAGE_KEYS:{CHAT_HISTORY:"nam_bot_chat_history_v3",THEME:"nam_bot_theme",SETTINGS:"nam_bot_settings"},MAX_MESSAGES:100,TYPING_DELAY:500};
